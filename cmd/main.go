f176fa69dd56e931e0c2435b12ee96cb0cba30b1
Johan Camilo Cortés Ocampo <camilo4182@gmail.com>
2025-03-23 18:26:04 -0500

    Made the connection to the database

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,31 @@
 package main
 
 import (
+	"log"
 	"os"
 	"os/signal"
 
+	"github.com/cortzero/go-postgres-blog/internal/data"
 	"github.com/cortzero/go-postgres-blog/internal/server"
 	"github.com/joho/godotenv"
 )
 
 func main() {
+	// Loading environment variables
 	godotenv.Load()
 	host := os.Getenv("HOST")
 	port := os.Getenv("PORT")
+
+	// Instantiating the server
 	serv := server.New(host, port)
 
+	// Instantiating a database connection
+	database := data.New()
+	if err := database.DB.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Starting the server
 	go serv.Start()
 
 	c := make(chan os.Signal, 1)
@@ -21,4 +33,5 @@ func main() {
 	<-c
 
 	serv.Close()
+	data.Close()
 }
--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.24.1
 require github.com/joho/godotenv v1.5.1
 
 require golang.org/x/crypto v0.36.0
+
+require github.com/lib/pq v1.10.9
